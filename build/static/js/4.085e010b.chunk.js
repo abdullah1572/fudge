335d27d8ef3aaf0b9a5a7a9429bd098b6ff1b34f
Sufyan <msufyan833@gmail.com>
2021-10-21 22:30:07 +0500

    build

---
+++ build/static/js/4.085e010b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpopencanvas=this.webpackJsonpopencanvas||[]).push([[4],{1055:function(n,p){}}]);
+//# sourceMappingURL=4.085e010b.chunk.js.map
--- build/static/js/4.5a76eded.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpopencanvas=this.webpackJsonpopencanvas||[]).push([[4],{1048:function(n,p){}}]);
-//# sourceMappingURL=4.5a76eded.chunk.js.map
