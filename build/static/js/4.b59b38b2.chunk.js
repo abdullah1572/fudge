9ba0a488289906172b3bcade7e9f3aa3dcdb8496
Sufyan <msufyan833@gmail.com>
2021-10-25 19:16:53 +0500

    new build

--- build/static/js/4.b59b38b2.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpfudge=this.webpackJsonpfudge||[]).push([[4],{1121:function(p,s){}}]);
-//# sourceMappingURL=4.b59b38b2.chunk.js.map
---
+++ build/static/js/4.bbda74fe.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfudge=this.webpackJsonpfudge||[]).push([[4],{1122:function(p,s){}}]);
+//# sourceMappingURL=4.bbda74fe.chunk.js.map
