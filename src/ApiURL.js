5969682c9f3b750b4cda227aa852d632ca176a5d
Kashmiri <waleed.shafiq96@gmail.com>
2021-11-05 15:57:31 +0500

    http to https

--- src/ApiURL.js
+++ src/ApiURL.js
@@ -1,2 +1,4 @@
-export const API_URL='http://159.89.237.13:5001'
+// export const API_URL='http://159.89.237.13:5001'
+export const API_URL='https://api.fudge.gallery'
+
 // export const API_URL='http://192.168.18.40:5001'
