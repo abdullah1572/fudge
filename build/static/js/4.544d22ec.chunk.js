3e4909113c52943e78ad448e017dc1ab7105be90
amanullah2 <amansheikh121@gmail.com>
2022-02-10 19:42:57 +0500

    build added

--- build/static/js/4.544d22ec.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpfudge=this.webpackJsonpfudge||[]).push([[4],{1167:function(n,u){},1208:function(n,u){},1275:function(n,u){}}]);
-//# sourceMappingURL=4.544d22ec.chunk.js.map
---
+++ build/static/js/4.a24666d3.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfudge=this.webpackJsonpfudge||[]).push([[4],{1282:function(p,s){}}]);
+//# sourceMappingURL=4.a24666d3.chunk.js.map
